a63284810e18250fb79722b3bf08539c3ab61705
bielzinhoooo <kingzinhomultiverse@gmail.com>
2023-07-30 11:20:50 -0300

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Sistema de cadastro de jogos</h1>
+
+>Status do projeto:Em desenvolvimento
+para rodar o projeto na sua maquina por favor presione:
+
+
+```
+node.js
+```
--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log ("aplicação esta se conectando ao banco de dados")
+let imagemdaestrada;
+
+function preload(){
+    imagemdaestrada = loadImage("imagens/estrada.png")
+}
+
+function setup() {
+    createCanvas(500, 400);
+}
+
+function draw() {
+    background(imagemdaestrada);
+} 
